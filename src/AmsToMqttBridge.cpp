f41f34299aa253f791671882f625334bf3e70308
Gunnar Skjold <gunnar.skjold@gmail.com>
2023-05-19 16:04:51 +0200

    DNS lookup test

--- src/AmsToMqttBridge.cpp
+++ src/AmsToMqttBridge.cpp
@@ -1,8 +1,6 @@
-#if defined(ESP8266)
-#include <ESP8266WiFi.h>
-#else
 #include <WiFi.h>
-#endif
+#include <lwip/dns.h>
+
 #define SSID "YOURSSIDHERE"
 #define PSK "YOURPASSWORDHERE"
 unsigned long lastSysout = 0;
@@ -12,15 +10,19 @@ void setup() {
 	Serial.setDebugOutput(true);
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(SSID, PSK);
+	WiFi.waitForConnectResult();
+	//WiFi.enableIpV6(); // This fixes this issue
 }
+
 void loop() {
 	unsigned long now = millis();
 	if(now-lastSysout > 1000) {
 		IPAddress dns1 = WiFi.dnsIP(0);
 		Serial.printf("DNS: %s\n", dns1.toString().c_str());
 		IPAddress res;
-		WiFi.hostByName("hub.amsleser.no", res);
-		Serial.printf("hub.amsleser.no -> %s", res.toString().c_str());
+		int ret = WiFi.hostByName("hub.amsleser.no", res);
+		Serial.printf("hub.amsleser.no -> %s (%d)\n", res.toString().c_str(), ret);
+		dns_clear_cache();
 		lastSysout = now;
 	}
 	delay(10);
