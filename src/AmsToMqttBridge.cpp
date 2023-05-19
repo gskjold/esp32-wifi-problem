d1d614aa84d2e0410dd1baa097122b2ea1600387
Gunnar Skjold <gunnar.skjold@gmail.com>
2023-05-19 08:32:36 +0200

    Update

--- src/AmsToMqttBridge.cpp
+++ src/AmsToMqttBridge.cpp
@@ -1,4 +1,8 @@
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
 #include <WiFi.h>
+#endif
 #define SSID "YOURSSIDHERE"
 #define PSK "YOURPASSWORDHERE"
 unsigned long lastSysout = 0;
@@ -11,10 +15,14 @@ void setup() {
 }
 void loop() {
 	unsigned long now = millis();
-	if(now-lastSysout > 30000) {
+	if(now-lastSysout > 1000) {
 		IPAddress dns1 = WiFi.dnsIP(0);
 		Serial.printf("DNS: %s\n", dns1.toString().c_str());
+		IPAddress res;
+		WiFi.hostByName("hub.amsleser.no", res);
+		Serial.printf("hub.amsleser.no -> %s", res.toString().c_str());
 		lastSysout = now;
 	}
+	delay(10);
 	yield();
 }
